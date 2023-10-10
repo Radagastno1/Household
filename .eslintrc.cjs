fa62e3d26cd9cb0b9e1eb3679cfde8e106a88713
Angelina Holmqvist <angelinaholmqvist@live.se>
2023-10-10 12:09:51 +0200

    eslint and prettier configured

---
+++ .eslintrc.cjs
@@ -0,0 +1,40 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "prettier",
+  ],
+  ignorePatterns: ["dist"],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {
+    "react/jsx-no-target-blank": "off",
+    "react/react-in-jsx-scope": "off",
+  },
+};
+
+//fråga david om vi behöver annat här
