52fbb0ca49bcb9e9edbc38996b30ee682f7b8146
Angelina Holmqvist <angelinaholmqvist@live.se>
2023-10-11 16:29:54 +0200

    we added some comments in the screens

---
+++ screens/HandleHouseholdScreen.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export default function HandleHouseholdScreen() {
+  return (
+    <View>
+      <Text>
+        CreateHouseholdscreen, här skapar vi ett nytt hushåll och får en enkel
+        kod. ELLER ange kod och gå med i befintligt hushåll.
+      </Text>
+    </View>
+  );
+}
--- screens/TaskDetailScreen.tsx
+++ screens/TaskDetailScreen.tsx
@@ -1,6 +1,9 @@
 import { View, Text } from "react-native";
 import React from "react";
 
+//här ser man detaljer om tasken. OM du är ägare över aktuella hushållet
+//DÅ kan du redigera tasken härifrån
+//man ska kunna välja sysslan som gjord
 export default function TaskDetailScreen() {
   return (
     <View>
