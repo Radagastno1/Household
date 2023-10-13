1cb8703999f76f3cacf1841cfa2a9e1752fa508b
Elin Nyman <elin.nyman87@gmail.com>
2023-10-13 21:45:37 +0200

    Fixed navigation stack and toptab

--- screens/CreateProfileScreen.tsx
+++ screens/CreateProfileScreen.tsx
@@ -1,1 +1,23 @@
+import { View, Text, StyleSheet, Button } from "react-native";
+import React from "react";
 
+export default function CreateProfileScreen({ navigation }: any) {
+  return (
+    <View style={styles.container}>
+      <Text>Här skapas en profil</Text>
+      <Button
+        title="Skapa profil"
+        onPress={() => navigation.navigate("HouseholdAccount")}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
--- screens/SignInScreen.tsx
+++ screens/SignInScreen.tsx
@@ -1,10 +1,27 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, Button, StyleSheet } from "react-native";
 
-export default function SignInScreen() {
+export default function SignInScreen({navigation}:any) {
   return (
-    <View>
-      <Text>SignInScreen, här loggar man in sig.</Text>
+    <View style={styles.container}>
+      <Text>Login Page</Text>
+      <Button
+        title="Need an account?"
+        onPress={() => navigation.navigate("Signup")}
+      />
+      <Button
+        title="Faked login"
+        onPress={() => navigation.navigate("HomeStack")}
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
