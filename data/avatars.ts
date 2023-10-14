3f6833f10a663911390bc3ce53d3c709a12a83b5
Angelina Holmqvist <angelinaholmqvist@live.se>
2023-10-14 22:48:44 +0200

    make a commit to test the webhook bot

--- data/avatars.ts
+++ data/avatars.ts
@@ -1,24 +1,23 @@
-
-
 export enum Avatars {
-    Bee = 'Bee',
-    Frog = 'Frog',
-    Monkey = 'Monkey',
-    Cat = 'Cat',
-    Koala = 'Koala',
-    Beetle = 'Beetle',
-    Fox = 'Fox',
-    Pig = 'Pig',
-  }
-  
-  export const AvatarColors: Record<Avatars, string> = {
-    [Avatars.Bee]: 'yellow',
-    [Avatars.Frog]: 'green',
-    [Avatars.Monkey]: 'brown',
-    [Avatars.Cat]: 'purple',
-    [Avatars.Koala]: 'grey',
-    [Avatars.Beetle]: 'red',
-    [Avatars.Fox]: 'orange',
-    [Avatars.Pig]: 'pink',
-  };
-  
+  Bee = "Bee",
+  Frog = "Frog",
+  Monkey = "Monkey",
+  Cat = "Cat",
+  Koala = "Koala",
+  Beetle = "Beetle",
+  Fox = "Fox",
+  Pig = "Pig",
+}
+
+export const AvatarColors: Record<Avatars, string> = {
+  [Avatars.Bee]: "yellow",
+  [Avatars.Frog]: "green",
+  [Avatars.Monkey]: "brown",
+  [Avatars.Cat]: "purple",
+  [Avatars.Koala]: "grey",
+  [Avatars.Beetle]: "red",
+  [Avatars.Fox]: "orange",
+  [Avatars.Pig]: "pink",
+};
+
+const avatarWithUrl = [];
