d8520cdcb9507c638f0a669f767c714c2e293c9e
Fengyan Tina <alwarzontina @gmail.com>
2023-10-15 11:01:36 +0200

    get the list rendered and link to add task

--- screens/HouseholdTasksScreen.tsx
+++ screens/HouseholdTasksScreen.tsx
@@ -1,13 +1,16 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
-import { TouchableOpacity, Image, Platform } from "react-native";
+import { TouchableOpacity, Image, Platform, ScrollView } from "react-native";
 import { Appbar, Card, Text, Button } from "react-native-paper";
 import { RootStackParamList } from "../navigators/RootNavigator";
 import { View, StyleSheet } from "react-native";
 import { profiles } from "../data/index";
 import { Profile } from "../types";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { TabBar, TabView } from "react-native-tab-view";
 import { AntDesign } from '@expo/vector-icons';
+import { households } from "../data";
+import { useAppDispatch, useAppSelector } from "../store/store";
+import { taskList } from "../store/tasks/taskSlice";
 // ska knna gå till lägg till ny task OM du är ägare för hushålllet
 //här listas alla sysslor i hushållet. nullas från avatarer varje midnatt.
 //vilka som gjort sysslan ska visas bredvid sysslan
@@ -16,14 +19,45 @@ import { AntDesign } from '@expo/vector-icons';
 
 export default function HouseholdTasksScreen(
   { navigation }: any,
-  profileId: string,
+ 
 ) {
+    const [profileId, setProfileId] = useState('profile1');
+    const profile = profiles.find((p)=>p.id == profileId);
+     const household = households.find((h)=>h.id == profile?.householdId)
+     const taskSlice = useAppSelector((state) => state.task);
+     const allTasksForHousehold = taskSlice.tasks.filter(
+       (t) => t.householdId === household?.id,
+     );
+     const dispatch = useAppDispatch();
+      // Use useSelector to access the profiles in your Redux state
+ 
+
+  // Find the specific profile by profileId
+ 
+
+//   useEffect(() => {
+//     if (profile && household) {
+//       // Dispatch the taskList action to fetch tasks and update the store
+//       // Replace with the actual logic to fetch tasks for the specific profile
+//     //   const fetchedTasks = yourApiCallToFetchTasksForProfile(profileId);
+//       dispatch(taskList(allTasksForHousehold));
+//     }
+//   }, [dispatch, profile, profileId]);
+
+
+    //  useEffect(() => {
+    //     // Dispatch the taskList action to fetch tasks and update the store
+    //     // Replace with the actual logic to fetch tasks from your API or wherever
+    //     const fetchedTasks = yourApiCallToFetchTasks();
+    //     dispatch(taskList(fetchedTasks));
+    //   }, [dispatch]);
+
   return (
     <View style={styles.container}>
       <Appbar.Header style={styles.customHeader}>
         {/* <Appbar.BackAction onPress={_backHome} /> */}
         <View style={styles.title}>
-          <Appbar.Content title="Househållet Namn" />
+          <Appbar.Content title={household?.name} />
         </View>
         <View style={styles.imageContainer}>
           <Appbar.Action
@@ -37,37 +71,22 @@ export default function HouseholdTasksScreen(
           />
         </View>
       </Appbar.Header>
-
-      <Card style={styles.card}>
+      <ScrollView style={styles.scrollContainer}>
+{allTasksForHousehold.map((task)=>(
+    <Card style={styles.card}>
         <View style={styles.taskItem}>
           <View>
-            <Text variant="titleLarge">Syssla1</Text>
+            <Text variant="titleLarge">{task.title}</Text>
           </View>
           <View>
             <Text variant="bodyMedium">avatarer1</Text>
           </View>
         </View>
       </Card>
-      <Card style={styles.card}>
-        <View style={styles.taskItem}>
-          <View>
-            <Text variant="titleLarge">Syssla2</Text>
-          </View>
-          <View>
-            <Text variant="bodyMedium">avatarer2</Text>
-          </View>
-        </View>
-      </Card>
-      <Card style={styles.card}>
-        <View style={styles.taskItem}>
-          <View>
-            <Text variant="titleLarge">Syssla3</Text>
-          </View>
-          <View>
-            <Text variant="bodyMedium">avatarer3</Text>
-          </View>
-        </View>
-      </Card>
+))}
+ </ScrollView>     
+      
+      
       <View style={styles.buttonContainer}>
         {/* {profile.isOwner &&( */}
        
@@ -76,7 +95,7 @@ export default function HouseholdTasksScreen(
             <AntDesign name="pluscircleo" size={20} color="black" />
           )}
           mode="outlined"
-          onPress={() => console.log("Lägg Till")}
+          onPress={() => navigation.navigate("HandleTask")}
           style={styles.button}
         >
           Lägg Till
@@ -99,6 +118,10 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     alignContent: "center",
+  },
+  scrollContainer: {
+    // Add any styling for the ScrollView container here
+    flex: 1,
   },
   imageContainer: {
     marginBottom: 20,
