6515ba2f92b2e488eb7a756d011d0f7c762ee94e
Bellefix <bellefixgustafsson@outlook.com>
2023-10-18 14:07:31 +0200

    en household slice med state för alla hushåll och state för aktivt hushåll

--- screens/HouseholdAccountScreen.tsx
+++ screens/HouseholdAccountScreen.tsx
@@ -1,7 +1,9 @@
 import React, { useEffect } from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
-import { useAppSelector } from "../store/store";
+import { useAppDispatch, useAppSelector } from "../store/store";
 import { Household } from "../types";
+import { Id } from "@reduxjs/toolkit/dist/tsHelpers";
+import { householdReducer, setHouseholdByHouseholdId, sethousehold } from "../store/household/householdSlice";
 
 const styles = StyleSheet.create({
   container: {
@@ -14,12 +16,18 @@ const styles = StyleSheet.create({
 
 export default function HouseholdAccountScreen({ navigation }: any) {
   const activeUser = useAppSelector((state) => state.userAccount.user);
+  const dispatch = useAppDispatch() 
   const connectedHouseholds = activeUser.households || []; // Default to an empty array if it's undefined
 
   useEffect(() => {
     // Hämta användarens hushåll när komponenten laddas
     // Implementera hämtning av hushåll här och uppdatera connectedHouseholds
   }, []);
+
+  const enterHousehold = (householdId: string) => {
+    dispatch(setHouseholdByHouseholdId({householdId: householdId}))
+    navigation.navigate("ProfileAccount")
+  };
 
   return (
     <View style={styles.container}>
@@ -33,7 +41,8 @@ export default function HouseholdAccountScreen({ navigation }: any) {
       />
       <Button
         title="Hushåll 1"
-        onPress={() => navigation.navigate("ProfileAccount")}
+        onPress={() => enterHousehold("household2")
+      }
       />
       <Button title="Logga ut" onPress={() => navigation.navigate("Auth")} />
     </View>
