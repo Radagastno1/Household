97ef2634bfa1ff3f2d8ba2407730f70db915be89
Elina Kerola <elinak90@icloud.com>
2023-10-19 12:02:57 +0200

    skapar ny profil till aktivt hushåll och hämtar alla profiler för avatarcheck

--- screens/CreateProfileScreen.tsx
+++ screens/CreateProfileScreen.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { AvatarColors, Avatars, AvatarUrls } from "../data/avatars";
 import { Button } from "react-native-paper";
 import { households } from "../data";
@@ -6,23 +6,13 @@ import { useDispatch } from 'react-redux';
 import { setProfile } from "../store/profile/profileSlice";
 import { Image } from "react-native";
 
-
-
-import {
-  Text,
-  View,
-  TextInput,
-  StyleSheet,
-  TouchableOpacity,
-} from "react-native";
+import { Text, View, TextInput, StyleSheet, TouchableOpacity } from "react-native";
 import { useTheme } from "../contexts/themeContext";
 import { useAppSelector } from "../store/store";
 
 type Avatar = {
   id: string;
 };
-
-
 
 const avatars: Avatar[] = [
   { id: Avatars.Bee },
@@ -36,32 +26,43 @@ const avatars: Avatar[] = [
 ];
 
 export default function CreateProfileScreen({ navigation, route }: any) {
-  
   const [householdName, setHouseholdName] = useState("");
   const [selectedAvatar, setSelectedAvatar] = useState<string | null>(null);
   const { id } = route.params;
   const household = households.find((h) => h.id === id);
-  const {theme } = useTheme();
+  const { theme } = useTheme();
   const dispatch = useDispatch();
   const todaysDate = new Date();
-  const activeUser = useAppSelector((state) => state.userAccount.user)
+  const activeUser = useAppSelector((state) => state.userAccount.user);
+
+  const activeProfiles = useAppSelector((state) => state.profile.profiles.filter((profile) => profile.householdId === id));
+
+  useEffect(() => {
+    // Fetch profiles for the active household if needed
+  }, [id, dispatch]);
+
+  const isAvatarOccupied = (avatarId: string) => {
+    return activeProfiles.some((profile) => profile.avatar === avatarId);
+  };
 
   const saveProfile = () => {
-    const avatar = selectedAvatar || '';
-    const avatarsColor = selectedAvatar ? AvatarColors[selectedAvatar as Avatars] : '';
-    const newProfile = {
-      id:  todaysDate.getUTCMilliseconds.toString().slice(-4),
-      profileName: householdName, 
-      userId: activeUser.id,
-      householdId: id,
-      avatar: avatar,
-      avatarsColors: avatarsColor,
-      isOwner: false,
-      isActive: false,
-    };
-     
-    dispatch(setProfile(newProfile));
-    navigation.navigate("HouseholdAccount");
+    console.log("hushållsid är", id)
+    if (selectedAvatar) {
+      const avatarsColor = AvatarColors[selectedAvatar as Avatars];
+      const newProfile = {
+        id: todaysDate.getUTCMilliseconds.toString().slice(-4),
+        profileName: householdName,
+        userId: activeUser.id,
+        householdId: id,
+        avatar: selectedAvatar,
+        avatarsColors: avatarsColor,
+        isOwner: false,
+        isActive: false,
+      };
+
+      dispatch(setProfile(newProfile));
+      navigation.navigate("HouseholdAccount");
+    }
   };
 
   return (
@@ -81,27 +82,29 @@ export default function CreateProfileScreen({ navigation, route }: any) {
         style={styles.input}
         onChangeText={(text) => setHouseholdName(text)}
       />
-<View style={styles.avatarsContainer}>
-  {avatars.map((avatar) => (
-    <TouchableOpacity
-      key={avatar.id}
-      style={[
-        styles.avatar,
-        selectedAvatar === avatar.id ? styles.selectedAvatar : undefined,
-        { backgroundColor: AvatarColors[avatar.id as Avatars] },
-      ]}
-      onPress={() => setSelectedAvatar(avatar.id as Avatars)}
-    >
-      <Image
-        source={{ uri: AvatarUrls[avatar.id as Avatars] }}
-        style={{ width: 40, height: 40 }}
-      />
-    </TouchableOpacity>
-  ))}
-</View>
-
-
-      <Button style={theme.button as any} onPress={saveProfile}>
+      <View style={styles.avatarsContainer}>
+        {avatars.map((avatar) => (
+          <TouchableOpacity
+            key={avatar.id}
+            style={[
+              styles.avatar,
+              selectedAvatar === avatar.id || isAvatarOccupied(avatar.id) ? styles.selectedAvatar : undefined,
+              { backgroundColor: AvatarColors[avatar.id as Avatars] },
+            ]}
+            onPress={() => {
+              if (!isAvatarOccupied(avatar.id)) {
+                setSelectedAvatar(avatar.id as Avatars);
+              }
+            }}
+          >
+            <Image
+              source={{ uri: AvatarUrls[avatar.id as Avatars] }}
+              style={{ width: 40, height: 40 }}
+            />
+          </TouchableOpacity>
+        ))}
+      </View>
+      <Button style={theme.button as any} onPress={saveProfile} disabled={!selectedAvatar}>
         <Text style={theme.buttonText}>Skapa</Text>
       </Button>
     </View>
@@ -142,7 +145,6 @@ const styles = StyleSheet.create({
     marginBottom: 150,
     padding: 10,
   },
-
   avatarsContainer: {
     flexDirection: "row",
     flexWrap: "wrap",
@@ -166,3 +168,4 @@ const styles = StyleSheet.create({
     fontSize: 18,
   },
 });
+
