933be4a949bb307ef54adda4bf2269bf17169a6d
Angelina Holmqvist <angelinaholmqvist@live.se>
2023-10-19 12:14:31 +0200

    task adds to database by first creating the document and getting that id and set to task id

--- api/task.ts
+++ api/task.ts
@@ -1,36 +1,26 @@
 import "firebase/firestore";
-import { addDoc, collection, getFirestore } from "firebase/firestore";
+import { addDoc, collection, doc, getFirestore } from "firebase/firestore";
 import { Task } from "../types";
 import { app } from "./config";
 
 const db = getFirestore(app);
 
-interface TaskData {
-  title: string;
-  description: string;
-  energiWeight: number;
-  creatingDate: string;
-  interval: number;
-  householdId: string;
-}
-
-function mapTaskToTaskDat(task: Task) {
-  const newTaskData: TaskData = {
-    title: task.title,
-    description: task.description,
-    energiWeight: task.energiWeight,
-    creatingDate: task.creatingDate,
-    interval: task.interval,
-    householdId: "fYHVLNiQvWEG9KNUGqBT",
-  };
-  return newTaskData;
-}
+//doc först  - skapa dokumentet för idt först
+//sen i docRef.id får man idt
+//sen add - det dokument id blir id i fältet
 
 export const addTaskToDB = async (task: Task) => {
+  task.householdId = "fYHVLNiQvWEG9KNUGqBT";
+
   const taskCollectionRef = collection(db, "tasks");
 
   try {
-    const taskRef = await addDoc(taskCollectionRef, mapTaskToTaskDat(task));
+    const docRef = await doc(taskCollectionRef);
+    //sätt docRef.id till task id
+    task.id = docRef.id;
+
+    const taskRef = await addDoc(taskCollectionRef, task);
+
     console.log("Uppgift tillagd med ID:", taskRef.id);
   } catch (error) {
     console.error("Fel vid tillägg av uppgift:", error);
