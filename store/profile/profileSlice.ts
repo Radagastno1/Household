fee41256a00e3f27f1ef79e235410c2351f78d25
Angelina Holmqvist <angelinaholmqvist@live.se>
2023-10-21 15:43:16 +0200

    new states in profileslice and a funcion fetchallprofiles by household

--- store/profile/profileSlice.ts
+++ store/profile/profileSlice.ts
@@ -1,6 +1,7 @@
 import { PayloadAction, createSlice } from "@reduxjs/toolkit";
-import { Profile } from "../../types";
+import { addProfileToDB, getAllProfilesByHouseholdId } from "../../api/profile";
 import { profiles } from "../../data";
+import { Profile } from "../../types";
 
 interface ProfileState {
   profiles: Profile[];
@@ -9,40 +10,83 @@ interface ProfileState {
 
 export const initialState: ProfileState = {
   profiles: profiles,
-  activeProfile: null
+  activeProfile: null,
 };
-
-
 
 const profileSlice = createSlice({
   name: "profile",
   initialState,
   reducers: {
-    setProfile: (state, action: PayloadAction<Profile>) => {
-      state.profiles = [...state.profiles, action.payload];
+    //denna ska anropas när man går in i ett hushåll, så man får alla profiler för det hushållet varje gång
+    setProfiles: (state, action: PayloadAction<Profile[]>) => {
+      state.profiles = action.payload;
+    },
+    //denna ska sättas när mnan får in i ett hushåll, vilken profil DU ÄR
+    setActiveProfile: (state, action: PayloadAction<(householdId:string, userId:string)) => {
+      const activeProfile = getProfileByHouseholdAndUser(action.payload).
+      then((activeProfile) => {
+        if (activeProfile) {
+          state.activeProfile = activeProfile;
+          console.log("aktiva profilen är: ", activeProfile.name);
+        }
+      })
+      .catch((error) => {
+        console.error("Fel vid tillägg av profil:", error);
+      });
+ 
     },
     // editProfileName: (state, action: PayloadAction<Profile>) => {
     //   state.profiles = [...state.profiles, action.payload];
     // },
-    editProfileName: (state, action: PayloadAction<{ profileId: string, newProfileName: string }>) => {
-      const profileToEdit = state.profiles.find(profile => profile.id === action.payload.profileId);
+    addProfile: (state, action: PayloadAction<Profile>) => {
+      addProfileToDB(action.payload)
+        .then((createdProfile) => {
+          if (createdProfile) {
+            state.profiles = [...state.profiles, createdProfile];
+            console.log("profile som las till: ", action.payload);
+          }
+        })
+        .catch((error) => {
+          console.error("Fel vid tillägg av profil:", error);
+        });
+    },
+    editProfileName: (
+      state,
+      action: PayloadAction<{ profileId: string; newProfileName: string }>,
+    ) => {
+      const profileToEdit = state.profiles.find(
+        (profile) => profile.id === action.payload.profileId,
+      );
       if (profileToEdit) {
         profileToEdit.profileName = action.payload.newProfileName;
       }
     },
-    setProfileByHouseholdAndUser:(state, action:PayloadAction<{userId:string, householdId:string}>) => {
-      const activeProfile = state.profiles.find(p => p.householdId === action.payload.householdId && p.userId === action.payload.userId)
+    setProfileByHouseholdAndUser: (
+      state,
+      action: PayloadAction<{ userId: string; householdId: string }>,
+    ) => {
+      const activeProfile = state.profiles.find(
+        (p) =>
+          p.householdId === action.payload.householdId &&
+          p.userId === action.payload.userId,
+      );
       if (activeProfile) {
         state.activeProfile = activeProfile;
       }
     },
-   
   },
 });
 
+export const fetchAllProfilesByHousehold =
+  (activeHouseholdId: string) => async (dispatch: any, getState: any) => {
+    const profiles = await getAllProfilesByHouseholdId(activeHouseholdId);
+    if (profiles) {
+      dispatch(profileSlice.actions.setProfiles(profiles));
+    }
+  };
 
-
-export const { setProfile } = profileSlice.actions;
+export const { setProfiles } = profileSlice.actions;
+export const { addProfile } = profileSlice.actions;
 export const { editProfileName } = profileSlice.actions;
 export const { setProfileByHouseholdAndUser } = profileSlice.actions;
 
