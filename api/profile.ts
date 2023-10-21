e2bbb528455e19622bb5faad017d25cf56c79134
Angelina Holmqvist <angelinaholmqvist@live.se>
2023-10-21 15:39:12 +0200

    addprofile and getprofiles by household fetch

---
+++ api/profile.ts
@@ -0,0 +1,110 @@
+import "firebase/firestore";
+import {
+  addDoc,
+  collection,
+  deleteDoc,
+  doc,
+  getDoc,
+  getDocs,
+  getFirestore,
+  query,
+  updateDoc,
+  where,
+} from "firebase/firestore";
+import { Profile, Task } from "../types";
+import { app } from "./config";
+
+const db = getFirestore(app);
+
+export const addProfileToDB = async (profile: Profile) => {
+  const profileCollectionRef = collection(db, "profiles");
+
+  try {
+    const docRef = await addDoc(profileCollectionRef, {});
+
+    profile.id = docRef.id;
+
+    console.log(
+      "Dokumentreferens id:",
+      docRef.id,
+      " och profilens id:",
+      profile.id,
+    );
+
+    await updateDoc(docRef, profile as Partial<Profile>);
+
+    const profileDoc = await getDoc(docRef);
+    if (profileDoc.exists()) {
+      const profileData = profileDoc.data();
+      return profileData as Profile;
+    } else {
+      console.error("Uppgiftsdokumentet finns inte i databasen.");
+      return null;
+    }
+  } catch (error) {
+    console.error("Fel vid tillägg av profil:", error);
+    return null;
+  }
+};
+
+export const getAllProfilesByHouseholdId = async (householdId: string) => {
+  try {
+    const profileCollectionRef = collection(db, "profiles");
+
+    const q = query(
+      profileCollectionRef,
+      where("householdId", "==", householdId),
+    );
+
+    const querySnapshot = await getDocs(q);
+
+    const profiles: Profile[] = [];
+
+    querySnapshot.forEach((doc) => {
+      profiles.push(doc.data() as Profile);
+    });
+
+    console.log("Profiler hämtade:", profiles);
+    return profiles;
+  } catch (error) {
+    console.error("Fel vid hämtning av uppgifter:", error);
+  }
+};
+
+// export const editTaskToDB = async (task: Task) => {
+//   task.householdId = "fYHVLNiQvWEG9KNUGqBT";
+//   const taskCollectionRef = collection(db, "tasks");
+
+//   try {
+//     const taskRef = doc(taskCollectionRef, task.id);
+
+//     const updatedTaskData = {
+//       id: task.id,
+//       title: task.title,
+//       description: task.description,
+//       energiWeight: task.energiWeight,
+//       creatingDate: task.creatingDate,
+//       interval: task.interval,
+//       householdId: task.householdId,
+//     };
+
+//     await updateDoc(taskRef, updatedTaskData);
+
+//     return task;
+//   } catch (error) {
+//     console.error("Fel vid redigering av uppgift:", error);
+//     return null;
+//   }
+// };
+
+// export const deleteTaskFromDB = async (taskId: string) => {
+//   //sen ska jag ta bort taskcompletions med??
+//   try {
+//     const taskDocRef = doc(db, "tasks", taskId);
+//     await deleteDoc(taskDocRef);
+
+//     console.log("Task borttagen med Id:", taskId);
+//   } catch (error) {
+//     console.error("Fel vid borttagning av tasken:", error);
+//   }
+// };
