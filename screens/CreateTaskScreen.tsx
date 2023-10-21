de00e6f695fe73d00f396993f2650c328a591296
Angelina Holmqvist <angelinaholmqvist@live.se>
2023-10-21 15:33:18 +0200

    fixed bug, you can edit tasks now. changed defaultvalue in inputs to value. and setting the values from task to edit in a useeffect

--- screens/CreateTaskScreen.tsx
+++ screens/CreateTaskScreen.tsx
@@ -12,6 +12,7 @@ import {
 } from "react-native";
 import { Card, Paragraph, Title } from "react-native-paper";
 import CircleComponent from "../components/CircleComponent";
+import { RootNavigationScreenProps } from "../navigators/navigationTypes";
 import { useAppDispatch, useAppSelector } from "../store/store";
 import {
   addTask,
@@ -20,41 +21,45 @@ import {
   filterTaskListByHouseId,
 } from "../store/tasks/taskSlice";
 import { Task } from "../types";
-import { RootNavigationScreenProps } from "../navigators/navigationTypes";
 
 type HandleTaskProps = RootNavigationScreenProps<"HandleTask">;
-//här skapar man en task som ägare för hushållet
-export default function CreateTaskScreen({ navigation, route }: HandleTaskProps) {
-  //LÅTSAS ATT JAG KOLLAR MOT HUSHÅLLSSTATET VILKET HUSHÅLL ANVÄNDAREN ÄR PÅ HÄR
+
+export default function CreateTaskScreen({
+  navigation,
+  route,
+}: HandleTaskProps) {
   const [isCreateMode, setIsCreateMode] = useState(true);
   const [taskToEdit, setTaskToEdit] = useState<Task>();
-  const householdSlice = useAppSelector((state) => state.household);
+  const activeHousehold = useAppSelector(
+    (state) => state.household.activehousehold,
+  );
 
-  const householdId = householdSlice.activehousehold?.id;
-  // const household = households.find((h) => h.id == householdId);
+  const householdId = activeHousehold?.id;
 
   const [intervalDataPressed, setIntervalDataPressed] = useState(false);
   const [energyDataPressed, setEnergyDataPressed] = useState(false);
 
-  const [selectedInterval, setSelectedInterval] = useState(
+  const [selectedInterval, setSelectedInterval] = useState<number>(
     taskToEdit ? taskToEdit.interval : 7,
   );
-  const [selectedEnergy, setSelectedEnergy] = useState(
-    taskToEdit ? taskToEdit.interval : 2,
+  const [selectedEnergy, setSelectedEnergy] = useState<number>(
+    taskToEdit ? taskToEdit.energiWeight : 2,
   );
 
-  const [title, setTitle] = useState("");
-  const [description, setDescription] = useState("");
+  const [title, setTitle] = useState<string>(
+    taskToEdit ? taskToEdit.title : "",
+  );
+  const [description, setDescription] = useState<string>(
+    taskToEdit ? taskToEdit.description : "",
+  );
 
   const dispatch = useAppDispatch();
 
-  //FÖR ATT TESTA HÄMTA TASKEN FÖR ETT HUSHÅLL SÅ JAG SER ATT DOM SKAPADES KORREKT
   const taskSlice = useAppSelector((state) => state.task);
 
   useEffect(() => {
     const { taskId } = route.params;
     const incomingTaskId = taskId;
-    console.log("taskid är: ", incomingTaskId);
     if (incomingTaskId == "0") {
       setIsCreateMode(true);
     } else {
@@ -62,6 +67,11 @@ export default function CreateTaskScreen({ navigation, route }: HandleTaskProps)
       if (taskToEdit) {
         setTaskToEdit(taskToEdit);
         setIsCreateMode(false);
+        setTitle(taskToEdit.title);
+        setDescription(taskToEdit.description);
+        setSelectedInterval(taskToEdit.interval);
+        setSelectedEnergy(taskToEdit.energiWeight);
+        console.log("task to edit interval:", taskToEdit.interval);
       } else {
         //navigera tillbaka?
       }
@@ -71,18 +81,6 @@ export default function CreateTaskScreen({ navigation, route }: HandleTaskProps)
   const intervalData: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   const energyData: number[] = [1, 2, 4, 6, 8];
 
-  //testar så att edit funkar
-  // const testEditTask = () => {
-  //   if (allTasksForHousehold.length >= 5) {
-  //     //I just pick the last one for now, but here it will be whatever task we are on in the detail screen
-  //     const lastTaskInList = allTasksForHousehold[5];
-  //     //here we edit the name for example to MATA KATTEN
-  //     const updatedTask = { ...lastTaskInList, title: "MATA KATTEN" };
-  //     //then dispatch to taskSlice
-  //     dispatch(editTask(updatedTask));
-  //   }
-  // };
-
   const handleDeleteTask = () => {
     if (taskToEdit) {
       dispatch(deleteTask(taskToEdit.id));
@@ -91,8 +89,6 @@ export default function CreateTaskScreen({ navigation, route }: HandleTaskProps)
   };
 
   const handleTask = () => {
-    const todaysDate = new Date();
-
     if (isCreateMode) {
       if (title && description && householdId) {
         const newTask: Task = {
@@ -109,7 +105,6 @@ export default function CreateTaskScreen({ navigation, route }: HandleTaskProps)
         dispatch(filterTaskListByHouseId({ household_Id: householdId }));
       }
     } else {
-      console.log("I ELSE");
       if (taskToEdit && householdId) {
         const editedTask: Task = {
           id: taskToEdit.id,
@@ -146,7 +141,7 @@ export default function CreateTaskScreen({ navigation, route }: HandleTaskProps)
             textAlignVertical="center"
             returnKeyType="done"
             onChangeText={(text) => setTitle(text)}
-            defaultValue={taskToEdit?.title}
+            value={title}
           ></TextInput>
           <TextInput
             placeholder="Beskrivning"
@@ -157,7 +152,7 @@ export default function CreateTaskScreen({ navigation, route }: HandleTaskProps)
             textAlignVertical="top"
             returnKeyType="done"
             onChangeText={(text) => setDescription(text)}
-            defaultValue={taskToEdit?.description}
+            value={description}
           ></TextInput>
           <Card style={styles.card}>
             <Card.Content style={styles.cardContent}>
@@ -176,19 +171,11 @@ export default function CreateTaskScreen({ navigation, route }: HandleTaskProps)
                   <TouchableOpacity
                     onPress={() => setIntervalDataPressed(true)}
                   >
-                    {isCreateMode ? (
-                      <CircleComponent
-                        number={selectedInterval}
-                        backgroundColor="red"
-                        color="white"
-                      />
-                    ) : (
-                      <CircleComponent
-                        number={taskToEdit?.interval ?? 0}
-                        backgroundColor="red"
-                        color="white"
-                      />
-                    )}
+                    <CircleComponent
+                      number={selectedInterval}
+                      backgroundColor="red"
+                      color="white"
+                    />
                   </TouchableOpacity>
                   <Title> dag</Title>
                 </View>
@@ -235,19 +222,11 @@ export default function CreateTaskScreen({ navigation, route }: HandleTaskProps)
                       setEnergyDataPressed(true);
                     }}
                   >
-                    {isCreateMode ? (
-                      <CircleComponent
-                        number={selectedEnergy}
-                        backgroundColor="lightgrey"
-                        color="black"
-                      />
-                    ) : (
-                      <CircleComponent
-                        number={taskToEdit?.energiWeight ?? 0}
-                        backgroundColor="lightgrey"
-                        color="black"
-                      />
-                    )}
+                    <CircleComponent
+                      number={selectedEnergy}
+                      backgroundColor="lightgrey"
+                      color="black"
+                    />
                   </TouchableOpacity>
                 </View>
               </View>
@@ -301,28 +280,6 @@ export default function CreateTaskScreen({ navigation, route }: HandleTaskProps)
             <AntDesign name="closecircleo" size={24} color="black" />
             <Text style={styles.buttonText}>Stäng</Text>
           </TouchableOpacity>
-        </View>
-
-        {/* TESTAR BARA ATT SKRIVA UT ALLA TASKS FÖR ETT HUSHÅLL SÅ JAG SER ATT DET FUNKAR*/}
-        <View style={{ flexDirection: "column" }}>
-          {/* <TouchableOpacity
-            style={{ padding: 20, backgroundColor: "red" }}
-            onPress={() => testEditTask()}
-          >
-            <Text>Redigera första tasken till att heta MATA KATT</Text>
-          </TouchableOpacity> */}
-          {/* {allTasksForHousehold
-            ? allTasksForHousehold.map((t) => (
-                <Text key={t.id}>
-                  Titel: {t.title}
-                  Beskrivning: {t.description}
-                  Energi: {t.energiWeight}
-                  Återkommande: {t.interval}
-                  HushållsId: {t.householdId}
-                  Datum: {t.creatingDate.toString()}
-                </Text>
-              ))
-            : null} */}
         </View>
       </ScrollView>
     </KeyboardAvoidingView>
