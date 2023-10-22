0d571b22cc6cc652284c68308a90a32b2236ebef
Angelina Holmqvist <angelinaholmqvist@live.se>
2023-10-22 22:00:00 +0200

    profiles from state instead of mockeddata in householdtaskscreen to get correct avatars.changed the days-difference-function a bit

--- screens/HouseholdTasksScreen.tsx
+++ screens/HouseholdTasksScreen.tsx
@@ -1,11 +1,10 @@
 import { AntDesign } from "@expo/vector-icons";
 import { useFocusEffect } from "@react-navigation/native";
-import React, { useCallback, useEffect } from "react";
-import { ScrollView, StyleSheet, View, Image } from "react-native";
+import React, { useCallback } from "react";
+import { Image, ScrollView, StyleSheet, View } from "react-native";
 import { Button, Card, Text } from "react-native-paper";
-import { households, profiles } from "../data";
+// import { profiles } from "../data";
 import { useAppDispatch, useAppSelector } from "../store/store";
-import { fetchCompletions } from "../store/taskCompletionSlice";
 import { fetchTasks, filterTaskListByHouseId } from "../store/tasks/taskSlice";
 import { Task } from "../types";
 
@@ -50,6 +49,9 @@ export default function HouseholdTasksScreen({
 
   // Use useSelector to access the profiles
   const activeProfile = useAppSelector((state) => state.profile.activeProfile);
+  const profilesForHousehold = useAppSelector(
+    (state) => state.profile.profiles,
+  );
   // const household = households.find((h) => h.id === activeProfile?.householdId);
   const taskSlice = useAppSelector((state) => state.task);
   const taskCompletionSlice = useAppSelector((state) => state.taskCompletion);
@@ -77,14 +79,19 @@ export default function HouseholdTasksScreen({
   };
 
   function findAllAvatarFortodayCompletionByTaskId(taskId: string) {
-    const today = new Date().toISOString();
+    const today = new Date();
+    console.log(
+      "ALLLLLAAAAA TASK COMPLETIONNSSSSSs:",
+      taskCompletionSlice.completions,
+    );
     //filter the completions with the same taskId---------can be moved out and share with getdays function
     const filteredTodaysCompletionsForTask =
       taskCompletionSlice.completions.filter(
-        (completion: any) =>
-          completion.completionDate.split("T")[0] === today.split("T")[0] &&
-          completion.taskId === taskId,
+        (completion) =>
+          completion.completionDate?.split("T")[0] ===
+            today.toISOString().split("T")[0] && completion.taskId === taskId,
       );
+
     // get the unique profileIds
     const uniqueProfileIds = [
       ...new Set(
@@ -95,7 +102,7 @@ export default function HouseholdTasksScreen({
     ];
     console.log(uniqueProfileIds);
     // profiles corresponding to the unique profileIds
-    const profilesForTask = profiles.filter((profile) =>
+    const profilesForTask = profilesForHousehold.filter((profile) =>
       uniqueProfileIds.includes(profile.id),
     );
 
@@ -104,37 +111,24 @@ export default function HouseholdTasksScreen({
   }
 
   function getDaysSinceLastCompletion(task: Task) {
-    let lastCompletionDate: Date;
-    const today = new Date().toISOString();
-    //all todays taskcompletions for task ---------can be moved out and share with getAvatar function
+    const today = new Date().toISOString().split("T")[0];
 
-    const filteredTodaysCompletionsForTask =
-      taskCompletionSlice.completions.filter(
-        (completion) =>
-          completion.taskId === task.id &&
-          completion.completionDate.split("T")[0] === today.split("T")[0],
-      );
+    const latestCompletion = taskCompletionSlice.completions
+      .filter((completion) => completion.taskId === task.id)
+      .reduce((latest, completion) => {
+        const completionDate = completion.completionDate.split("T")[0];
+        return completionDate > latest ? completionDate : latest;
+      }, "");
 
-    if (filteredTodaysCompletionsForTask.length === 0) {
-      //if it is empty, no one did it today
-      lastCompletionDate = new Date(task.creatingDate);
-    } else {
-      //get the latest date it was done
-      lastCompletionDate = new Date(
-        filteredTodaysCompletionsForTask.slice().sort((a, b) => {
-          return (
-            new Date(b.completionDate).getTime() -
-            new Date(a.completionDate).getTime()
-          );
-        })[0].completionDate,
-      );
-    }
-    // get the timeDifference since last done date
-    const currentDate = new Date();
-    const timeDifference = currentDate.getTime() - lastCompletionDate.getTime();
-    //convert it to days minus the interval days
-    const daysDifference =
-      Math.floor(timeDifference / (1000 * 60 * 60 * 24)) - task.interval;
+    const currentDate = new Date().toISOString().split("T")[0];
+    const lastCompletionDate =
+      latestCompletion || task.creatingDate.split("T")[0];
+
+    const timeDifference =
+      Date.parse(currentDate) - Date.parse(lastCompletionDate);
+    const daysDifference = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+
+    console.log("days difference: ", daysDifference);
     return daysDifference;
   }
 
@@ -145,17 +139,6 @@ export default function HouseholdTasksScreen({
           isOwner ? styles.scrollContainerOwner : styles.scrollContainerNonOwner
         }
       >
-        {/* <View>
-          {taskCompletionSlice.completions.map((c) => (
-            <Text
-              key={c.id}
-              style={{ backgroundColor: "blue", color: "white" }}
-            >
-              PROFILID: {c.profileId} TASKID: {c.taskId}
-            </Text>
-          ))}
-        </View> */}
-
         {taskSlice.filteredTasks.map((task) => (
           <Card
             key={task.id}
