0abf056e3d44994e2b9f654d71db60953db0b242
Bellefix <bellefixgustafsson@outlook.com>
2023-10-23 13:42:23 +0200

    pillat i household api samt funktionalitet på handlehouseholdscreen

--- screens/HandleHouseholdScreen.tsx
+++ screens/HandleHouseholdScreen.tsx
@@ -1,22 +1,42 @@
-import { View, Text, StyleSheet, Button } from "react-native";
 import React from "react";
+import { View, Text, StyleSheet, Button } from "react-native";
 import { RootNavigationScreenProps } from "../navigators/navigationTypes";
 
-type HandleHousholdProps = RootNavigationScreenProps<"HandleHousehold">;
+// Genererar en slumpmässig kod
+export const generateHouseholdCode = (length: number = 6) => {
+  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let code = "";
 
-export default function HandleHouseholdScreen({
-  navigation,
-}: HandleHousholdProps) {
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    code += characters.charAt(randomIndex);
+  }
+
+  return code;
+};
+
+type HandleHouseholdProps = RootNavigationScreenProps<"HandleHousehold">;
+
+  export default function HandleHouseholdScreen({
+    navigation,
+  }: HandleHouseholdProps) {
+    const createNewHousehold = () => {
+      // Generera en kod för det nya hushållet
+      const householdCode = generateHouseholdCode();
+      
+      // Navigera till CreateProfileScreen med kodinfo
+      navigation.navigate("CreateProfile", {
+        householdId: householdCode,
+      });
+    }
+    
+
   return (
     <View style={styles.container}>
       <Text>Här skapas ett hushåll</Text>
       <Button
         title="Skapa hushåll"
-        onPress={() =>
-          navigation.navigate("CreateProfile", {
-            householdId: "fYHVLNiQvWEG9KNUGqBT",
-          })
-        }
+        onPress={createNewHousehold}
       />
       <Button
         title="Stäng"
