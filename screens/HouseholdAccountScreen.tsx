f99b6a408d2c9e983b7abae89435b2bf1ba9cd4d
Elin Nyman <elin.nyman87@gmail.com>
2023-10-30 20:39:19 +0100

    Revert "Minor css-changes"
    This reverts commit a9c809fdb612b56aa8e652188de7898c6bc232eb.

--- screens/HouseholdAccountScreen.tsx
+++ screens/HouseholdAccountScreen.tsx
@@ -1,103 +1,95 @@
-import { useFocusEffect } from "@react-navigation/native";
-import React, { useCallback, useEffect, useState } from "react";
-import { Appearance, Text, View } from "react-native";
-import CreateHouseholdButton from "../components/CreateHouseholdButton";
-import Header from "../components/Header";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import React from "react";
+import { Image, Text, TouchableOpacity, View } from "react-native";
 import { useTheme } from "../contexts/themeContext";
-import { RootNavigationScreenProps } from "../navigators/navigationTypes";
-import { getHouseholdsByHouseholdIdAsync } from "../store/household/householdSlice";
-import { getProfilesByUserIdAsync } from "../store/profile/profileSlice";
-import { getRequestByHouseholdIdsAsync } from "../store/request/requestSlice";
+import { AvatarUrls, Avatars } from "../data/avatars";
+import { sethouseholdActive } from "../store/household/householdSlice";
+import {
+  fetchAllProfilesByHousehold,
+  setProfileByHouseholdAndUser,
+} from "../store/profile/profileSlice";
 import { useAppDispatch, useAppSelector } from "../store/store";
-import { User } from "../types";
-import ModeThemeButton from "../components/ModeThemeButton";
-import LogoutButton from "../components/LogoutButton";
-import HouseholdList from "../components/HouseholdList";
+import { Household, HouseholdRequest, Profile, User } from "../types";
 
-type HouseholdProps = RootNavigationScreenProps<"HouseholdAccount">;
+interface HouseholdItemProps {
+  index: number;
+  household: Household;
+  profile: Profile;
+  request: HouseholdRequest;
+  activeUser: User;
+}
 
-export default function HouseholdAccountScreen({ navigation }: HouseholdProps) {
-  const [systemTheme, setSystemTheme] = useState(Appearance.getColorScheme());
-  const [profilesLoaded, setProfilesLoaded] = useState(false);
-  const [requestsLoaded, setRequestsLoaded] = useState(false);
-
-  const activeUser = useAppSelector((state) => state.user.user);
-  const activeProfile = useAppSelector((state) => state.profile.activeProfile);
-  const profilesToUser = useAppSelector(
-    (state) => state.profile.profilesToUser,
-  );
-  const households = useAppSelector((state) => state.household.households);
-  const requests = useAppSelector((state) => state.request.requests);
-  const dispatch = useAppDispatch();
-
+export default function HouseholdItem({
+  index,
+  household,
+  profile,
+  request,
+  activeUser,
+}: HouseholdItemProps) {
   const { theme } = useTheme();
-  let householdIds: string[] = [];
+  const dispatch = useAppDispatch();
+  const activeProfile = useAppSelector((state) => state.profile.activeProfile);
 
-  useEffect(() => {
-    dispatch(getProfilesByUserIdAsync(activeUser?.uid ?? "hej")).then(() => {
-      setProfilesLoaded(true);
-    });
-    householdIds = profilesToUser.map((p) => p.householdId);
-  }, [!profilesLoaded]);
-
-  useEffect(() => {
-    dispatch(getHouseholdsByHouseholdIdAsync(householdIds));
-  }, [profilesLoaded]);
-
-  useFocusEffect(
-    useCallback(() => {
-      dispatch(getRequestByHouseholdIdsAsync(householdIds)).then(() => {
-        setRequestsLoaded(true);
-      });
-    }, [profilesLoaded, !requestsLoaded]),
-  );
-
-  useEffect(() => {
-    if (activeProfile) {
-      navigation.navigate("ProfileAccount", {
-        householdId: activeProfile.householdId,
-      });
+  const handleEnterHousehold = async (household: Household) => {
+    dispatch(sethouseholdActive(household));
+    console.log("HHOUSEHOLDID ÄR: ", household.id);
+    try {
+      dispatch(fetchAllProfilesByHousehold(household.id, activeUser!.uid));
+      dispatch(
+        setProfileByHouseholdAndUser({
+          userId: activeUser!.uid,
+          householdId: household.id,
+        }),
+      );
+      console.log("FRÅN HOUSEHOLDACCOUNT: ", activeProfile?.id);
+      console.log("aktiva profilen: ", activeProfile);
+    } catch (error) {
+      console.error("Error entering household:", error);
     }
-  }, [activeProfile]);
+  };
 
   return (
-    <View
-      style={{
-        flex: 1,
-        backgroundColor: theme.colors.background,
-        justifyContent: "center",
+    <TouchableOpacity
+      key={index}
+      style={[
+        theme.cardButton as any,
+        { flexDirection: "row", justifyContent: "space-between" },
+      ]}
+      onPress={() => {
+        handleEnterHousehold(household);
       }}
     >
-      <Header text={"Välkommen"} />
       <View
         style={{
-          flex: 1,
-          marginTop: 0,
+          flexDirection: "row",
           alignItems: "center",
-          justifyContent: "center",
-          paddingTop: 20,
         }}
       >
-        <Text style={[theme.buttonText]}>MINA HUSHÅLL</Text>
-        <HouseholdList
-          households={households}
-          profilesToUser={profilesToUser}
-          requests={requests}
-          activeUser={activeUser as User}
+        <Image
+          key={0}
+          source={{
+            uri: AvatarUrls[profile?.avatar as Avatars],
+          }}
+          style={{ height: 20, width: 20 }}
+          alt={`Avatar ${index}`}
         />
       </View>
-      <View
-        style={{
-          flex: 1,
-          alignItems: "center",
-          justifyContent: "center",
-          paddingTop: 20,
-        }}
-      >
-        <CreateHouseholdButton navigation={navigation} />
-        <LogoutButton />
-        <ModeThemeButton />
+
+      <View>
+        <Text style={theme.buttonText}>{household.name}</Text>
       </View>
-    </View>
+
+      <View style={{ flexDirection: "row", gap: 5 }}>
+        {request && profile?.isOwner === true ? (
+          <MaterialCommunityIcons
+            name="bell-alert-outline"
+            size={24}
+            color="black"
+          />
+        ) : (
+          <View style={{ width: 24 }}></View>
+        )}
+      </View>
+    </TouchableOpacity>
   );
 }
