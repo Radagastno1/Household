2446f32175bea56ae282b676c74925c2621e598d
Jessica Trapp <jessica.maria.trapp@gmail.com>
2023-10-30 12:07:30 +0100

    removed Name, changed Användarnamn to email and removed acceptera villkoren

--- screens/CreateUserAccountScreen.tsx
+++ screens/CreateUserAccountScreen.tsx
@@ -3,14 +3,13 @@ import {
   StyleSheet,
   Text,
   TouchableOpacity,
-  TouchableWithoutFeedback,
   View,
   useColorScheme,
 } from "react-native";
-import { Checkbox, Modal, Portal, TextInput } from "react-native-paper";
+import { TextInput } from "react-native-paper";
 import { useTheme } from "../contexts/themeContext";
 import { RootNavigationScreenProps } from "../navigators/navigationTypes";
-import { useAppDispatch, useAppSelector } from "../store/store";
+import { useAppDispatch } from "../store/store";
 import { addUserAsync } from "../store/user/userSlice";
 import { UserCreate } from "../types";
 
@@ -19,14 +18,10 @@ type CreateUserProps = RootNavigationScreenProps<"Signup">;
 export default function CreateUserAccountScreen({
   navigation,
 }: CreateUserProps) {
-  const [visible, setVisible] = useState(false);
-  const [showWarning, setShowWarning] = useState(false);
   const [passwordVisible, setPasswordVisible] = useState(false);
-  const [isChecked, setIsChecked] = useState(false);
   const [passwordMismatchWarning, setPasswordMismatchWarning] = useState("");
   const [missingFieldsWarning, setMissingFieldsWarning] = useState("");
-  const [newName, setNewName] = useState("");
-  const [newUserName, setNewUserName] = useState("");
+  const [newEmail, setNewEmail] = useState("");
   const [newPassword, setNewPassword] = useState("");
   const [confirmationPasswordVisible, setConfirmationPasswordVisible] =
     useState(false);
@@ -38,30 +33,20 @@ export default function CreateUserAccountScreen({
 
   const dispatch = useAppDispatch();
 
-  const showModal = () => setVisible(true);
-  const hideModal = () => setVisible(false);
-
   const handleCreateAccount = async () => {
-    if (!isChecked) {
-      setShowWarning(true);
-    } else if (newPassword !== confirmationPasswordInput) {
+    if (newPassword !== confirmationPasswordInput) {
       setPasswordMismatchWarning("Lösenordet matchar inte");
-    } else if (!newName || !newUserName || !newPassword) {
+    } else if (!newEmail || !newPassword) {
       setMissingFieldsWarning("Fyll i alla obligatoriska fält.");
     } else {
       console.log("ELSE");
       const newUser: UserCreate = {
-        // displayName: newName,
-        email: newUserName,
+        email: newEmail,
         password: newPassword,
       };
       dispatch(addUserAsync(newUser));
       navigation.navigate("Login");
     }
-  };
-
-  const toggleCheckbox = () => {
-    setIsChecked(!isChecked);
   };
 
   return (
@@ -73,30 +58,18 @@ export default function CreateUserAccountScreen({
 
         <TextInput
           label={
-            !newName && missingFieldsWarning ? (
-              <Text style={{ color: "red" }}>Namn*</Text>
-            ) : (
-              "Namn"
-            )
-          }
-          style={styles.input}
-          value={newName}
-          onChangeText={setNewName}
-        />
-        <TextInput
-          label={
-            !newUserName && missingFieldsWarning ? (
+            !newEmail && missingFieldsWarning ? (
               <Text style={{ color: colorScheme === "dark" ? "black" : "red" }}>
-                <Text style={{ color: "red" }}>Användarnamn*</Text>
-                Användarnamn"
+                <Text style={{ color: "red" }}>Email</Text>
+                Email"
               </Text>
             ) : (
-              "Användarnamn"
+              "Email"
             )
           }
           style={styles.input}
-          value={newUserName}
-          onChangeText={setNewUserName}
+          value={newEmail}
+          onChangeText={setNewEmail}
         />
         <TextInput
           label={
@@ -144,43 +117,6 @@ export default function CreateUserAccountScreen({
           <Text style={styles.warning}>{passwordMismatchWarning}</Text>
         ) : null}
 
-        <TouchableWithoutFeedback onPress={toggleCheckbox}>
-          <View style={styles.checkboxContainer}>
-            <Checkbox status={isChecked ? "checked" : "unchecked"} />
-            <Text style={styles.checkboxText}>
-              Jag accepterar{" "}
-              <Text
-                style={{ color: "blue", textDecorationLine: "underline" }}
-                onPress={showModal}
-              >
-                villkoren
-              </Text>
-            </Text>
-          </View>
-        </TouchableWithoutFeedback>
-
-        <Portal>
-          <Modal visible={visible} onDismiss={hideModal}>
-            <View style={styles.modalContainer}>
-              <Text>Här står villkoren</Text>
-            </View>
-          </Modal>
-        </Portal>
-
-        <Portal>
-          <Modal visible={showWarning} onDismiss={() => setShowWarning(false)}>
-            <View style={styles.warningContainer}>
-              <Text>Du måste acceptera villkoren för att skapa konto.</Text>
-              <TouchableOpacity
-                onPress={() => setShowWarning(false)}
-                style={styles.okButton}
-              >
-                <Text style={{ color: "black" }}>OK</Text>
-              </TouchableOpacity>
-            </View>
-          </Modal>
-        </Portal>
-
         <TouchableOpacity
           style={theme.button as any}
           onPress={handleCreateAccount}
@@ -220,29 +156,6 @@ const styles = StyleSheet.create({
     padding: 5,
     fontSize: 16,
     color: "black",
-  },
-  checkboxContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-  },
-  checkboxText: {
-    fontSize: 16,
-    color: "black",
-  },
-  modalContainer: {
-    backgroundColor: "white",
-    padding: 5,
-  },
-  warningContainer: {
-    backgroundColor: "white",
-    padding: 5,
-  },
-  okButton: {
-    backgroundColor: "yellow",
-    padding: 5,
-    alignItems: "center",
-    margin: 10,
-    borderRadius: 10,
   },
   warning: {
     color: "red",
