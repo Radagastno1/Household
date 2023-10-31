bea6a3bd6a5b9d061c535b15b181b0fde466a63a
Elin Nyman <elin.nyman87@gmail.com>
2023-10-31 12:17:08 +0100

    Data for empty charts done

---
+++ data/emptyPieCharts.ts
@@ -0,0 +1,34 @@
+export const emptyPieChartsData = [
+  {
+    colors: ["#b7b6b6", "#a09f9e", "#8b8a89"],
+    series: [10, 10, 10],
+    title: "Diska",
+  },
+  {
+    colors: ["#b7b6b6", "#a09f9e", "#8b8a89"],
+    series: [10, 5, 8],
+    title: "Laga mat",
+  },
+  {
+    colors: ["#b7b6b6", "#a09f9e", "#8b8a89"],
+    series: [4, 6, 2],
+    title: "Dammsuga",
+  },
+  {
+    colors: ["#b7b6b6", "#a09f9e", "#8b8a89"],
+    series: [2, 2, 4],
+    title: "Mata fisken",
+  },
+  {
+    colors: ["#b7b6b6", "#a09f9e", "#8b8a89"],
+    series: [10, 10, 10],
+    title: "Tvätta",
+  },
+];
+
+export const emptySumPieChart = [
+  {
+    colors: ["#b7b6b6", "#a09f9e", "#8b8a89"],
+    series: [36, 33, 34],
+  },
+];
