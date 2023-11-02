9635b14806d8724e58cae10dcd131fa7cd8cf45f
Elin Nyman <elin.nyman87@gmail.com>
2023-11-02 21:16:07 +0100

    Added a errormessage to signup-screen for email

--- screens/CreateUserAccountScreen.tsx
+++ screens/CreateUserAccountScreen.tsx
@@ -10,6 +10,7 @@ import {
 } from "react-native";
 import { TextInput } from "react-native-paper";
 import { useTheme } from "../contexts/themeContext";
+import ErrorModule from "../modules/errorModule";
 import { RootNavigationScreenProps } from "../navigators/navigationTypes";
 import { useAppDispatch } from "../store/store";
 import { addUserAsync } from "../store/user/userSlice";
@@ -22,8 +23,10 @@ export default function CreateUserAccountScreen({
 }: CreateUserProps) {
   const [passwordVisible, setPasswordVisible] = useState(false);
   const [passwordMismatchWarning, setPasswordMismatchWarning] = useState("");
+  const [error, setError] = useState(false);
+  const [errorPopup, setErrorPopup] = useState(false);
   const [missingFieldsWarning, setMissingFieldsWarning] = useState("");
-  const [newEmail, setNewEmail] = useState("");
+  const [newEmail, setNewEmail] = useState<string>("");
   const [newPassword, setNewPassword] = useState("");
   const [confirmationPasswordVisible, setConfirmationPasswordVisible] =
     useState(false);
@@ -40,6 +43,9 @@ export default function CreateUserAccountScreen({
       setPasswordMismatchWarning("Lösenordet matchar inte");
     } else if (!newEmail || !newPassword) {
       setMissingFieldsWarning("Fyll i alla obligatoriska fält.");
+    } else if (!isValidEmail(newEmail)) {
+      setError(true);
+      setErrorPopup(true);
     } else {
       const newUser: UserCreate = {
         email: newEmail,
@@ -47,6 +53,10 @@ export default function CreateUserAccountScreen({
       };
       dispatch(addUserAsync(newUser));
     }
+  };
+
+  const isValidEmail = (newEmail: string) => {
+    return newEmail.includes("@") && newEmail.includes(".");
   };
 
   return (
@@ -139,6 +149,16 @@ export default function CreateUserAccountScreen({
             </TouchableOpacity>
           </View>
         </View>
+        {error && (
+          <ErrorModule
+            errorMessage={"Ange en giltig email"}
+            buttonMessage="Försök igen"
+            onClose={() => {
+              setError(false);
+              setErrorPopup(false);
+            }}
+          />
+        )}
       </View>
     </KeyboardAvoidingView>
   );
